5aef97959225a35976075cf3b4b50ab5faecce10
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2015-06-10 10:02:11 -0700

    Changed env var for mongo

--- app.rb
+++ app.rb
@@ -6,7 +6,7 @@ require 'json'
 
 include Mongo
 
-mongo_url = ENV['MONGOHQ_URL'] || 'mongodb://localhost/dbname'
+mongo_url = ENV['MONGOLAB_URI'] || 'mongodb://localhost/dbname'
  
 MongoMapper.connection = Mongo::Connection.from_uri mongo_url
 MongoMapper.database = URI.parse(mongo_url).path.gsub(/^\//, '') #Extracts 'dbname' from the uri
