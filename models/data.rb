3af109afe41e5e73edf6d4a993c77ebab8a95dc3
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2015-06-17 22:00:55 -0700

    Data for pics is stored in mongo

--- models/data.rb
+++ models/data.rb
@@ -1,19 +1,19 @@
 require 'mongo'
 require 'mongo_mapper'
 
-class Data
+class PicData
 	include MongoMapper::Document
 
-	key :pic_name, String, required: true
-	key :main_categories, Array, required: true
-  key :sub_categories, Array, required: true
-  key :pic_url, String, required: true
-  key :pic_question_id, Integer, required: true
-  key :question0, Hash, required: true
-  key :question1, Hash, required: true
-  key :question2, Hash, required: true
-  key :question3, Hash, required: true
-  key :question4, Hash, required: true
+	key :pic_name, String
+	key :main_categories, Array
+  key :sub_categories, Array
+  key :pic_url, String
+  key :pic_question_id, Integer
+  key :question0, Hash
+  key :question1, Hash
+  key :question2, Hash
+  key :question3, Hash
+  key :question4, Hash
   timestamps!
 
 end
