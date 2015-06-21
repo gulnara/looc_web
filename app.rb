4ab691f4c5114695545812e8c0380a3b44d53b3c
Gulnara Mirzakarimova <gmirzaka@gmail.com>
2015-06-21 12:46:38 -0700

    Hiding access token in ENV

--- app.rb
+++ app.rb
@@ -38,20 +38,14 @@ Warden::Strategies.add(:password) do
   end
 end
 
-# Implement your Warden stratagey to validate and authorize the access_token.
+
 Warden::Strategies.add(:access_token) do
     def valid?
-        # Validate that the access token is properly formatted.
-        # Currently only checks that it's actually a string.
         request.env["HTTP_ACCESS_TOKEN"].is_a?(String)
     end
 
     def authenticate!
-        # Authorize request if HTTP_ACCESS_TOKEN matches 'youhavenoprivacyandnosecrets'
-        # Your actual access token should be generated using one of the several great libraries
-        # for this purpose and stored in a database, this is just to show how Warden should be
-        # set up.
-        access_granted = (request.env["HTTP_ACCESS_TOKEN"] == 'youhavenoprivacyandnosecrets')
+        access_granted = (request.env["HTTP_ACCESS_TOKEN"] == ENV['SECRET_TOKEN'])
         !access_granted ? fail!("Could not log in") : success!(access_granted)
     end
 end
